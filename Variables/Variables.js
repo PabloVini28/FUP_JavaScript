2d5841463b317272d9da1619f55ebd32684f442f
Pablo Vinícios <128994831+PabloVini28@users.noreply.github.com>
2024-09-20 22:24:22 -0300

    Add files via upload

---
+++ Variables/Variables.js
@@ -0,0 +1,13 @@
+var Nome = 'Pablo';
+// Não é boa praticar usar var, pois ela é global e pode ser redeclarada
+
+let Nome1 = 'Pablo';
+
+// let é uma variável local, ou seja, ela só pode ser usada dentro do escopo onde foi declarada
+const numero = 5;
+
+// const é uma constante, ou seja, ela não pode ser alterada
+
+console.log(Nome);
+console.log(Nome1);
+console.log(numero);
