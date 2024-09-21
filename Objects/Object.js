b6f7671e20d5fca853dac3b332ad1f6d07d62413
Pablo Vinícios <128994831+PabloVini28@users.noreply.github.com>
2024-09-21 14:39:12 -0300

    Add files via upload

---
+++ Objects/Object.js
@@ -0,0 +1,15 @@
+// Objeto no JavaScript é uma coleção de dados e/ou funcionalidades relacionadas (que geralmente consistem em diversas variáveis e funções — que são chamadas de propriedades e métodos quando estão dentro de objetos).
+
+const Pessoa = {
+    nome: 'João',
+    idade: 30,  
+    
+    fale(){
+        console.log('Olá');
+    },
+    fale2(){
+        console.log('Olá 2');
+    }
+}
+
+Pessoa.fale();
