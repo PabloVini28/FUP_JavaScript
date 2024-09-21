76a5d6d122e2bd8ec9d8448811d05355bb322a8b
Pablo Vinícios <128994831+PabloVini28@users.noreply.github.com>
2024-09-20 23:33:43 -0300

    Add files via upload

---
+++ Operators/Operators.js
@@ -0,0 +1,30 @@
+// Operadores Aritméticos em JavaScript
+// Adição: + (soma)    
+// Subtração: - (subtração)
+// Multiplicação: * (multiplicação)
+// Divisão: / (divisão)
+// Módulo: % (resto da divisão)
+
+const num1 = 5;
+console.log(num1 + 2);
+console.log(num1 - 2);
+console.log(num1 * 2);
+console.log(num1 / 2);
+console.log(num1 % 2);
+
+// Potenciação
+console.log(5**2);
+
+// Incremento
+let num2 = 5;
+console.log(num2++);
+console.log(num2);
+console.log(++num2);
+console.log(num2);
+
+// Decremento
+let num3 = 5;
+console.log(num3--);
+console.log(num3);
+console.log(--num3);
+console.log(num3);
