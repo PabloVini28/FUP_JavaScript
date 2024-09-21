f710c9bca6c63fac6aa49d3df966ee543ed4a1d5
Pablo Vinícios <128994831+PabloVini28@users.noreply.github.com>
2024-09-20 23:48:54 -0300

    Add files via upload

---
+++ Functions/function.js
@@ -0,0 +1,16 @@
+function saudacao(){
+    console.log('bom dia');
+}
+
+saudacao();
+
+function soma(a, b){
+    console.log(a+b);
+}
+
+soma(5,5);
+
+function multiplicacao(a, b){
+    return a*b;
+}
+console.log(multiplicacao(5,5)); //retorna o valor da multiplicação
